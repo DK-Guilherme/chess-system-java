245f73da6573468fd7657ed77375fdb2c98647fc
Dark Kill <guilhermegsouza520@gmail.com>
2021-10-15 14:31:25 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -18,7 +18,7 @@ public class ChessMatch {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for (int i=0; i<board.getRows(); i++) {
 			for (int j=0; j<board.getColumns(); j++) {
-				mat[i][j] = (ChessPiece) board.pieces(i, j);
+				mat[i][j] = (ChessPiece) board.piece(i, j);
 			}
 		}
 		return mat;
